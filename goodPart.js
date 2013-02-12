3ff9adb2845226c769013c5bab53fd4f984a675a
williammw <william.manwai@gmail.com>
2013-02-13 00:15:36 +0800

    second-commit

---
+++ goodPart.js
@@ -0,0 +1,18 @@
+var flight = {
+
+	airline : "Ocenaic",
+	number : 77,
+	departure :{
+		IATA: "SYD",
+		time:"2013-02-09 0950",
+		city:"Aus"
+	},
+	arrival :{
+		IATA: "LAC",
+		time:"2013-01-09 1950",
+		city:"Tyko"
+
+	}
+}
+
+console.log(flight.departure.time);
